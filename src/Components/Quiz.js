d395d3dfa93ee3a8ae68296e8312d7bbd43cc9b9
Pipetboy <felipefelizx@gmail.com>
2023-01-14 00:49:26 -0300

    feature: 3 preguntas y muestra score

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -9,34 +9,37 @@ const Quiz = () => {
     const [currentQuestion, setCurrentQuestion] = useState({});
     const [CountryData, setCountryData] = useState(countryData);
     const [answer, setAnswer] = useState('');
+    // Create state for questions array and score
+    const [questions, setQuestions] = useState([]);
+    const [score, setScore] = useState(0);
 
     useEffect(() => {
         axios.get('https://restcountries.com/v2/all')
             .then(response => {
                 console.log(response.data);
                 setCountryData(response.data);
-                generateRandomQuestion();
+                generateRandomQuestions();
             })
             .catch(error => {
                 console.log(error)
             });
     }, []);
 
-    function generateRandomQuestion() {
-        // Generar pregunta al azar utilizando los datos de la API
+    function generateRandomQuestions() {
+        // Generar preguntas al azar utilizando los datos de la API
         if (CountryData && CountryData.length) {
-            const randomIndex = Math.floor(Math.random() * CountryData.length);
-            const randomCountry = CountryData[randomIndex];
-            const question = {
-                text: `What is the capital of ${randomCountry.name}?`,
-                correctAnswer: randomCountry.capital
-,
-
-                options: [randomCountry.capital
-, randomCountry.altSpellings[1], randomCountry.region, randomCountry.subregion]
+            for (let i = 0; i < 4; i++) {
+                const randomIndex = Math.floor(Math.random() * CountryData.length);
+                const randomCountry = CountryData[randomIndex];
+                const question = {
+                    text: `What is the capital of ${randomCountry.name}?`,
+                    correctAnswer: randomCountry.capital,
+                    options: [randomCountry.capital, randomCountry.altSpellings[1], randomCountry.region, randomCountry.subregion]
+                }
+                questions.push(question);
             }
-            setCurrentQuestion(question);
-            console.log(question);
+            setQuestions(questions);
+            setCurrentQuestion(questions[0]);
         }
 
     }
@@ -46,16 +49,26 @@ const Quiz = () => {
 
     function checkAnswer() {
         if (answer === currentQuestion.correctAnswer) {
-            setQuizCompleted(true);
+            setScore(score + 1);
         } else {
             alert("Incorrect answer");
         }
+        // Move to next question
+        const currentIndex = questions.findIndex(q => q === currentQuestion);
+        if (currentIndex + 1 < questions.length) {
+            setCurrentQuestion(questions[currentIndex + 1]);
+        } else {
+            setQuizCompleted(true);
+        }
+        setAnswer('');
     }
     function restartQuiz() {
         setQuizCompleted(false);
-        setCurrentQuestion(null);
+        setQuestions([]);
+        setCurrentQuestion({});
         setAnswer('');
-        generateRandomQuestion();
+        setScore(0);
+        generateRandomQuestions();
     }
     return (
         <div className='card'>
@@ -64,11 +77,12 @@ const Quiz = () => {
                 <button onClick={() => {
                     if (!quizCompleted) {
                         axios.get('https://restcountries.com/v2/all')
-                            .then(response => {
-                                console.log(response.data);
-                                setCountryData(response.data);
-                                generateRandomQuestion();
-                            })
+                            .then(
+                                response => {
+                                    console.log(response.data);
+                                    setCountryData(response.data);
+                                    generateRandomQuestions();
+                                })
                             .catch(error => {
                                 console.log(error)
                             });
@@ -77,7 +91,7 @@ const Quiz = () => {
                     }
                 }}>Start/Restart Quiz</button>
             </div>
-            {countryData.length && !quizCompleted && currentQuestion.text ? (
+            {CountryData.length && !quizCompleted && currentQuestion.text ? (
                 <div>
                     <h2>{currentQuestion.text}</h2>
                     {currentQuestion.options.map((option, index) => (
@@ -90,7 +104,7 @@ const Quiz = () => {
             ) : (
                 quizCompleted && (
                     <div>
-                        <h2>Quiz Completed!</h2>
+                        <h2>Quiz Completed! Score: {score}</h2>
                     </div>
                 )
             )}
