0494410a22f81f6dc61bafdee559f497c0e44da2
Pipetboy <felipefelizx@gmail.com>
2023-01-15 17:18:47 -0300

    fix: Solo una correcta

--- src/Components/OptionButton.js
+++ src/Components/OptionButton.js
@@ -1,7 +1,7 @@
 const OptionButton = ({ option, onClick, isSelected, isCorrect }) => {
     return (
         <button
-            className={`Answer ${isSelected ? 'selected' : ''} ${isCorrect ? 'correct' : ''} ${!isCorrect && isSelected ? 'incorrect' : ''}`}
+            className={`Answer ${isSelected ? 'selected' : ''} ${isCorrect && isSelected ? 'correct' : ''} ${!isCorrect && isSelected ? 'incorrect' : ''}`}
             onClick={() => onClick(option)}>
             {option}
         </button>
